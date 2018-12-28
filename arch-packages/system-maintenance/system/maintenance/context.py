a31b03636a0eb1ca819da39852fb0b327bed4e43
radium <r@di.um>
2018-12-28 16:02:50 +0100

    Fix

--- arch-packages/system-maintenance/system/maintenance/context.py
+++ arch-packages/system-maintenance/system/maintenance/context.py
@@ -18,8 +18,8 @@ class Context:
             "playbook_file_path": "provision.yml",
             "git_repo_url": "https://github.com/radium226/odroid-xu4.git",
             "folder_path": "ansible",
-            "requirements_file_path": "requirements.yml", 
-            "galaxy_roles_path": "galaxy-roles"
+            "requirements_file_path": "requirements.yml",
+            "roles_folder_path": "galaxy-roles"
         }
     }
 
