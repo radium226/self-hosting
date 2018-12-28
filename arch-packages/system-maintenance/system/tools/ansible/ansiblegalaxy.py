ec2c8d86d186e48655a6dbb6a91196078b46ec22
radium <r@di.um>
2018-12-28 16:09:14 +0100

    Fix

--- arch-packages/system-maintenance/system/tools/ansible/ansiblegalaxy.py
+++ arch-packages/system-maintenance/system/tools/ansible/ansiblegalaxy.py
@@ -10,7 +10,8 @@ class AnsibleGalaxy:
     def install_requirements(self, requirements_file_path, roles_folder_path, force=False):
         requirements_args = ["-r", str(requirements_file_path)]
         roles_path_args = ["-p", str(roles_folder_path)]
-        command = ["ansible-galaxy"] + requirements_args + roles_path_args
+        action_args = ["install"]
+        command = ["ansible-galaxy"] + action_args + requirements_args + roles_path_args
         #print(f"command={command}")
         process = Popen(command)
         return_code = process.wait()
